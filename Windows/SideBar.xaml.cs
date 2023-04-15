2f418cd60a5cf87383b01c9aa30d7f410b9d835c
BlackNetik <mrbatya.vova@yandex.ru>
2023-04-15 23:55:54 +0300

    Добавлен Side Navigation Bar, делающийся по инструкции на ютубе: https://youtu.be/zfl1aE6zl20 (1:24)

---
+++ Windows/SideBar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourseLearning.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для SideBar.xaml
+    /// </summary>
+    public partial class SideBar : Window
+    {
+        public SideBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
