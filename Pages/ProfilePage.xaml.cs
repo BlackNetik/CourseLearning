5ccf52fc31fec597833e5629aba990bf1141a96d
BlackNetik <mrbatya.vova@yandex.ru>
2023-05-13 11:18:52 +0300

    Примерный вид сертификата. Осталось только добавить сохранение файла через проводник и вставить иконку приложения в конец

--- Pages/ProfilePage.xaml.cs
+++ Pages/ProfilePage.xaml.cs
@@ -22,6 +22,11 @@ using System.Windows.Shapes;
 using static System.Net.Mime.MediaTypeNames;
 using System.Xml.Linq;
 using Microsoft.Office.Interop.Word;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.IO;
+using DocumentFormat.OpenXml.Drawing.Wordprocessing;
 
 namespace CourseLearning.Pages
 {
@@ -32,6 +37,7 @@ namespace CourseLearning.Pages
     {
         //private ProfilePageViewModel viewModel;
         string ConnectionToBD = "Server=localhost; port=5432; user id=postgres; password=password; database=courselearning;";
+        string imagePath = @"C:\Users\User\Desktop\c#\CourseLearning\Images\OutputWord\CourseLearning.jpg";
         User ProfileUser { get; set; }
 
         public ProfilePage(User user)
@@ -76,6 +82,7 @@ namespace CourseLearning.Pages
             var button = sender as Button;
             var row = button?.DataContext as DataRowView;
 
+
             // Check if the row is not null and display the ID in a MessageBox
             if (row != null)
             {
@@ -83,66 +90,103 @@ namespace CourseLearning.Pages
                 string courseName = row["course_name"].ToString();
                 MessageBox.Show($"The selected course is {courseName}");
                 */
-                AddImageAndTextToWordDocument();
+                //AddImageAndTextToWordDocument();
+                CourseProgress course = new CourseProgress()
+                {
+                    Id = 0, //Int32.Parse(row["id"].ToString())
+                    UserId = ProfileUser.Id,
+                    CourseName = row["course_name"].ToString(),
+                    Progress = Int32.Parse(row["progress"].ToString()),
+                    Status = row["status_course"].ToString()
+                };
+
+                CreateCertificate(ProfileUser, course,imagePath);
             }
         }
+        
+        
 
-        public void AddImageAndTextToWordDocument()
+        public void CreateCertificate(User user, CourseProgress courseProgress, string imagePath)
         {
-            // Открываем проводник и предлагаем выбрать путь для Word файла
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Word Documents (*.docx)|*.docx";
-            saveFileDialog.DefaultExt = "docx";
-            saveFileDialog.AddExtension = true;
-            if (saveFileDialog.ShowDialog() != true)
+            string fileName = $"Certificate_{user.FirstName}_{user.LastName}_{courseProgress.CourseName}.docx";
+            using (WordprocessingDocument document = WordprocessingDocument.Create(fileName, WordprocessingDocumentType.Document))
             {
-                return;
+                MainDocumentPart mainPart = document.AddMainDocumentPart();
+                mainPart.Document = new DocumentFormat.OpenXml.Wordprocessing.Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+
+                // Добавление иконки приложения
+                ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+                using (FileStream stream = new FileStream(imagePath, FileMode.Open))
+                {
+                    imagePart.FeedData(stream);
+                }
+
+                string imageId = mainPart.GetIdOfPart(imagePart);
+
+                // Добавление содержимого сертификата
+                DocumentFormat.OpenXml.Wordprocessing.Paragraph titleParagraph = new DocumentFormat.OpenXml.Wordprocessing.Paragraph(new DocumentFormat.OpenXml.Wordprocessing.Run(new DocumentFormat.OpenXml.Wordprocessing.Text("Сертификат о прохождении курса")));
+                titleParagraph.ParagraphProperties = new DocumentFormat.OpenXml.Wordprocessing.ParagraphProperties(new DocumentFormat.OpenXml.Wordprocessing.Justification() { Val = DocumentFormat.OpenXml.Wordprocessing.JustificationValues.Center });
+                body.AppendChild(titleParagraph);
+
+                DocumentFormat.OpenXml.Wordprocessing.Paragraph courseNameParagraph = new DocumentFormat.OpenXml.Wordprocessing.Paragraph(new DocumentFormat.OpenXml.Wordprocessing.Run(new DocumentFormat.OpenXml.Wordprocessing.Text($"Курс: {courseProgress.CourseName}")));
+                body.AppendChild(courseNameParagraph);
+
+                DocumentFormat.OpenXml.Wordprocessing.Paragraph userNameParagraph = new DocumentFormat.OpenXml.Wordprocessing.Paragraph(new DocumentFormat.OpenXml.Wordprocessing.Run(new DocumentFormat.OpenXml.Wordprocessing.Text($"Имя: {user.FirstName} {user.LastName}")));
+                body.AppendChild(userNameParagraph);
+
+                DocumentFormat.OpenXml.Wordprocessing.Paragraph progressParagraph = new DocumentFormat.OpenXml.Wordprocessing.Paragraph(new DocumentFormat.OpenXml.Wordprocessing.Run(new DocumentFormat.OpenXml.Wordprocessing.Text($"Процент прохождения: {courseProgress.Progress}%")));
+                body.AppendChild(progressParagraph);
+
+
+                // Добавление изображения
+                //AddImageToBody(body, mainPart, imageId);
+
+                mainPart.Document.Save();
             }
-
-            // Создаем новое приложение Word
-            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
-            wordApp.Visible = true;
-
-            // Добавляем новый документ
-            Microsoft.Office.Interop.Word.Document doc = wordApp.Documents.Add();
-
-            // Добавляем изображение
-            string imagePath = @"C:\Users\User\Desktop\c#\CourseLearning\Images\OutputWord\CourseLearning.jpg";
-            //"/Images/OutputWord/CourseLearning.jpg"
-            //@"C:\Users\User\Desktop\c#\CourseLearning\Images\OutputWord\CourseLearning.jpg"
-            object oMissed = doc.Paragraphs[1].Range;
-            object oLinkToFile = false;
-            object oSaveWithDocument = true;
-            object oRange = doc.Paragraphs[1].Range;
-            Microsoft.Office.Interop.Word.InlineShape inlineShape = doc.InlineShapes.AddPicture(imagePath, ref oLinkToFile, ref oSaveWithDocument, ref oRange);
-            inlineShape.Width = 100;
-            inlineShape.Height = 100;
-
-            // Добавляем текст
-            /*
-            Microsoft.Office.Interop.Word.Range range = doc.Paragraphs.Add().Range;
-            range.Text = "Привет Word";
-            */
-            // Add text between the pictures
-            object oCollapseEnd = WdCollapseDirection.wdCollapseEnd;
-            Range rng = doc.Paragraphs[1].Range;
-            rng.Collapse(ref oCollapseEnd);
-            rng.Text = "This is the text between the pictures.";
-            rng.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphJustify; // Align the text to the width
-
-
-
-            // Сохраняем документ
-            object oFileName = saveFileDialog.FileName;
-            object oFileFormat = Microsoft.Office.Interop.Word.WdSaveFormat.wdFormatDocumentDefault;
-            doc.SaveAs2(ref oFileName, ref oFileFormat);
-
-            // Закрываем документ и приложение Word
-            ((_Document)doc).Close();
-            ((_Application)wordApp).Quit();
-
-            MessageBox.Show("Документ успешно сохранен.");
         }
+
+        private void AddImageToBody(Body body, MainDocumentPart mainPart, string imageId)
+        {
+            DocumentFormat.OpenXml.Wordprocessing.Run run = new DocumentFormat.OpenXml.Wordprocessing.Run();
+            RunProperties runProperties = new RunProperties();
+            DocumentFormat.OpenXml.Wordprocessing.Drawing drawing = new DocumentFormat.OpenXml.Wordprocessing.Drawing();
+
+            DocumentFormat.OpenXml.Drawing.Wordprocessing.Inline inline = new DocumentFormat.OpenXml.Drawing.Wordprocessing.Inline();
+            inline.AnchorId = "anchorId";
+            inline.DocProperties = new DocProperties() { Id = 1, Name = "Picture" };
+            inline.Extent = new Extent() { Cx = 990000L, Cy = 792000L };
+
+            DocumentFormat.OpenXml.Drawing.Graphic graphic = new DocumentFormat.OpenXml.Drawing.Graphic();
+            graphic.GraphicData = new DocumentFormat.OpenXml.Drawing.GraphicData() { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" };
+
+            DocumentFormat.OpenXml.Drawing.Pictures.Picture picture = new DocumentFormat.OpenXml.Drawing.Pictures.Picture();
+            picture.BlipFill = new DocumentFormat.OpenXml.Drawing.Pictures.BlipFill();
+            picture.BlipFill.Blip = new DocumentFormat.OpenXml.Drawing.Blip() { Embed = imageId };
+            picture.BlipFill.SourceRectangle = new DocumentFormat.OpenXml.Drawing.SourceRectangle();
+            //picture.BlipFill.Blip.Scratch = new DocumentFormat.OpenXml.Drawing.Stretch();
+            //picture.BlipFill.Blip.Stretch.FillRectangle = new DocumentFormat.OpenXml.Drawing.FillRectangle();
+
+            picture.ShapeProperties = new DocumentFormat.OpenXml.Drawing.Pictures.ShapeProperties();
+            picture.ShapeProperties.Transform2D = new DocumentFormat.OpenXml.Drawing.Transform2D();
+            picture.ShapeProperties.Transform2D.Extents = new DocumentFormat.OpenXml.Drawing.Extents() { Cx = 990000L, Cy = 792000L };
+            picture.ShapeProperties.Transform2D.Offset = new DocumentFormat.OpenXml.Drawing.Offset() { X = 0L, Y = 0L };
+            //picture.ShapeProperties.PresetGeometry = new DocumentFormat.OpenXml.Drawing.Pictures.PresetGeometry() { Preset = DocumentFormat.OpenXml.Drawing.ShapeTypeValues.Rectangle };
+            //picture.ShapeProperties.PresetGeometry.AdjustValueList = new DocumentFormat.OpenXml.Drawing.AdjustValueList();
+
+
+
+            graphic.GraphicData.Append(picture);
+            //graphic.Append(graphic.GraphicData);
+            drawing.Append(inline);
+            run.Append(runProperties);
+            run.Append(drawing);
+
+            DocumentFormat.OpenXml.Wordprocessing.Paragraph imageParagraph = new DocumentFormat.OpenXml.Wordprocessing.Paragraph(run);
+            imageParagraph.ParagraphProperties = new ParagraphProperties(new Justification() { Val = JustificationValues.Center });
+            body.AppendChild(imageParagraph);
+        }
+
     }
 }
 
